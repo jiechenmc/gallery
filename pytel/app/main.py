72a1a3f79cd22a3ca489e29c7d988d1a6bbe7e50
Jie Chen <jiechenmc@gmail.com>
2025-05-12 00:36:10 -0400

    add pyroscope

--- pytel/app/main.py
+++ pytel/app/main.py
@@ -1,3 +1,5 @@
+import pyroscope
+
 from app.routers import users
 from fastapi import FastAPI
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
@@ -7,6 +9,11 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+pyroscope.configure(
+  application_name = "pytel", # replace this with some name for your application
+  server_address   = "http://pyroscope:4040", # replace this with the address of your Pyroscope server
+)
 
 app = FastAPI()
 
